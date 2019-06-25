1dd77cef36e0d68cc1f07f48878acb4a39838fe0
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2019-06-25 18:37:56 -0400

    Update camera_button.py

--- Boggle_AI/camera_button.py
+++ Boggle_AI/camera_button.py
@@ -4,10 +4,28 @@
 import os
 import time
 import datetime
-import importlib
+import RPi.GPIO as GPIO
+import sys
 
-importlib.import_module(simple_button)
-importlib.import_module(pi_camera)
+class simple_button(object):
+	"""Class for use with an simple click button
+	"""
+	def __init__(self, function, *args, pin=7, pin_setup='BOARD', **kwargs):
+		self.pin = pin
+		if pin_setup == 'BCM':
+			GPIO.setmode(GPIO.BCM)
+		else:
+			GPIO.setmode(GPIO.BOARD)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #set for use with internal pull up resistor.  If using a resistor on a bread board this can be removed.
+		GPIO.remove_event_detect(self.pin)
+		GPIO.add_event_detect(self.pin, GPIO.RISING, callback=lambda x: self.callback(function, *args, **kwargs))
+		
+	def callback(self, function, *args, **kwargs):
+		if GPIO.input(self.pin) == 1:
+			function(*args, **kwargs)
+		else:
+			pass
 
 if __name__ == '__main__':
 	try:
