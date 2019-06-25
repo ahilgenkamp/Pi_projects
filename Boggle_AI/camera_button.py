a78f6dbd1c147f7ec90b1b1c0abeade35ea9b858
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2019-06-25 19:16:13 -0400

    Update camera_button.py

--- Boggle_AI/camera_button.py
+++ Boggle_AI/camera_button.py
@@ -1,6 +1,6 @@
 #python program to take picture when button is pressed
 from modules.button import simple_button
-#from .modules.pi_camera import pi_camera
+from modules.pi_camera import pi_camera
 import os
 import time
 import datetime
@@ -8,26 +8,25 @@ import sys
 
 if __name__ == '__main__':
 	try:
-		'''
 		#create function to pass when button is pressed
 		def take_pic(save_path):
 			file_name = datetime.datetime.now()+'.jpg'
 			pi_camera.capture(save_path=save_path+'/'+file_name, t=0.5)
 			print('Picture Taken')
-		'''
 		cwd = os.getcwd()
+		'''
 		def print_test():
 			print(cwd)
-		
+
 		simple_button(print_test, pin=17, pin_setup='BCM')	
+		'''
+		simple_button(take_pic, cwd+"/boggle_images/", pin=17, pin_setup='BCM')
 		while True:
 			time.sleep(0.5)
 
 	except KeyboardInterrupt:
 		print('\n\n *** Stopping Program ***')
 		try:
-			GPIO.cleanup()
 			sys.exit(0)
 		except SystemExit:
-			GPIO.cleanup()
 			os._exit(0)
