b492acf4159d7e9d0839b2159f50aff76e7ad586
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2019-06-25 19:19:51 -0400

    Update camera_button.py

--- Boggle_AI/camera_button.py
+++ Boggle_AI/camera_button.py
@@ -9,9 +9,10 @@ import sys
 if __name__ == '__main__':
 	try:
 		#create function to pass when button is pressed
+		cam = pi_camera()
 		def take_pic(save_path):
 			file_name = str(datetime.datetime.now())+'.jpg'
-			pi_camera().capture(save_path=save_path+'/'+file_name, t=0.5)
+			cam.capture(save_path=save_path+'/'+file_name, t=0.5)
 			print('Picture Taken')
 		cwd = os.getcwd()
 		'''
