280dea3737def225ac1d15828e330b8e219fa4c8
ahilgenkamp <ahilgie@gmail.com>
2019-06-26 17:29:20 -0400

    word dist updates

--- Boggle_AI/word_distribution.py
+++ Boggle_AI/word_distribution.py
@@ -1,13 +1,25 @@
 #word distribution graph
 
+import string
 import matplotlib
 from nltk.corpus import words
 
 #load dictionary
 word_list = words.words()
 
-print(len(word_list))
+print('Total Words: '+str(len(word_list)))
 
-for word in word_list[0:20]:
-	print(word)
+low = string.ascii_lowercase
+'''
+for i in range(len(low)):
+	num_letters = sum(a.lower() == low[i] for a, *_ in word_list)  #for notes on *_  --> https://www.python.org/dev/peps/pep-3132/
+	print(low[i].upper()+': '+str(num_letters))
+'''
+two_letters = []
+for i in low:
+	for j in low:
+		two_letters.append(i+j)
 
+for i in range(len(two_letters[0:35])):
+	num_letters = sum(a[0:2].lower() == two_letters[i] for a in word_list)  #for notes on *_  --> https://www.python.org/dev/peps/pep-3132/
+	print(two_letters[i].upper()+': '+str(num_letters))
